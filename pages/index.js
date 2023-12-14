7406319eb896b0a749bdd5ef59413970d2792336
Ibuki Uehara <n23003@std.it-college.ac.jp>
2023-12-14 05:26:31 +0000

    absolute path import
    インポートを絶対パスでできるように修正

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Header from '../components/header.js'
-import Hero from '../components/hero.js'
-import Footer from '../components/footer.js'
+import Header from 'components/header.js'
+import Hero from 'components/hero.js'
+import Footer from 'components/footer.js'
 
 const Home = () => {
   return (
