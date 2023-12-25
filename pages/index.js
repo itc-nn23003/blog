a2ff20d82f41261f50e94912d6a706f0c2093b79
Ibuki Uehara <n23003@std.it-college.ac.jp>
2023-12-25 03:29:22 +0000

    6.4
    サイトに関する情報を共有できる形で用意する

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero.js'
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <>
       <Container>
+        <Meta />
         <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
       </Container>
     </>
